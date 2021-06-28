49929884ff0b8aa0a662264f96e596a8b1f03cb2
sukshmata <84189259+sukshmata@users.noreply.github.com>
2021-06-28 15:36:01 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Header() {
+  return <header>ATHMAKURI SUKSHMATA</header>;
+}
+
+export default Header;
---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p> A basic bootcamp on webdevelopment with help of react.js</p>
+    </div>
+  );
+}
+
+export default Info;
